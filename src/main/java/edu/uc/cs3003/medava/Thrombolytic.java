c388a1b4fc67d53c6048928a84360606e078efe6
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2025-12-02 15:38:28 -0500

    Update Thrombolytic.java

--- src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
+++ src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
@@ -1,1 +1,33 @@
 package edu.uc.cs3003.medava;
+
+public class Thrombolytic extends Medicine
+{
+    public Thrombolytic()
+    {
+        super("Thrombolytic");
+    }
+
+    @Override
+    public double minimumTemperature()
+    {
+        System.out.print(
+            "Getting the minimum safe temperature for a Thrombolytic drug.\n"
+        );
+        return 39.2;
+    }
+
+    @Override
+    public double maximumTemperature()
+    {
+        System.out.print(
+            "Getting the maximum safe temperature for a Thrombolytic drug.\n"
+        );
+        return 41.0;
+    }
+
+    @Override
+    public MedicineSchedule getSchedule()
+    {
+        return MedicineSchedule.Uncontrolled;
+    }
+}
