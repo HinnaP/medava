9ad13f29d2270fa60c7979730aa61ae1f4842269
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2025-12-02 15:31:21 -0500

    Update MedicineSchedule.java

--- src/main/java/edu/uc/cs3003/medava/MedicineSchedule.java
+++ src/main/java/edu/uc/cs3003/medava/MedicineSchedule.java
@@ -1,1 +1,22 @@
 package edu.uc.cs3003.medava;
+
+public enum MedicineSchedule {
+    One(1), Two(2), Three(3), Four(4), Five(5), Uncontrolled(6);
+
+    private final int scheduleId;
+
+    MedicineSchedule(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String asString() {
+        switch (scheduleId) {
+            case 1 -> { return "Schedule One Medicine"; }
+            case 2 -> { return "Schedule Two Medicine"; }
+            case 3 -> { return "Schedule Three Medicine"; }
+            case 4 -> { return "Schedule Four Medicine"; }
+            case 5 -> { return "Schedule Five Medicine"; }
+            default -> { return "Uncontrolled"; }
+        }
+    }
+}
