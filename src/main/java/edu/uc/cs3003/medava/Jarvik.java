8acbb4897b69ac4f610ca96feb28181814657801
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2025-12-02 23:51:34 -0500

    Update Jarvik.java

--- src/main/java/edu/uc/cs3003/medava/Jarvik.java
+++ src/main/java/edu/uc/cs3003/medava/Jarvik.java
@@ -1,28 +1,26 @@
 package edu.uc.cs3003.medava;
 
-public class Jarvik implements Shippable{
-    Jarvik(String deviceSerialNumber) {
-      serialNumber = deviceSerialNumber;
-    }
-  
+public abstract class Medicine implements Shippable {
+    public abstract MedicineSchedule getSchedule();
+    private String mMedicineName;
     public String getMedicineName() {
-      return "Jarvik Artificial Heart";
-    }
-  
-    public MedicineSchedule getSchedule() {
-      return MedicineSchedule.Uncontrolled;
-    }
-  
-    public String getSerialNumber() {
-      return serialNumber;
-    }
-  
-    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
-      if (30.0 <= lowTemperature && highTemperature <= 90.0) {
-        return true;
+        return mMedicineName;
       }
-      return false;
+    public Medicine(String medicineName) {
+         mMedicineName = medicineName;
     }
-  
-    private String serialNumber;
-  }
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+                highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+}
