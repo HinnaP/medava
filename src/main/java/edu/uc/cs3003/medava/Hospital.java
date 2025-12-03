c15b4f237b5f16f7d780a73ab23856c517c2796b
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2025-12-02 23:48:50 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,30 +1,35 @@
 package edu.uc.cs3003.medava;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class Hospital {
 
     private String name;
 
     public Hospital(String hospitalName) {
-        name = hospitalName;
+        this.name = hospitalName;
     }
 
-    /**
-     * Receives shipments from a transporter.
-     * Note: Reflection-based implementation is preserved in comments for reference.
-     */
-    void receive(Transporter t) {
-        while (!t.isEmpty()) {
-            Shippable unloaded = t.unload();
-            System.out.printf("Checking whether Hospital can receive %s.\n", unloaded.getMedicineName());
-            
-            if (unloaded.getSchedule() != MedicineSchedule.Uncontrolled) {
-                System.out.printf("Hospital cannot receive controlled substances and %s is a %s.\n",
-                        unloaded.getMedicineName(), unloaded.getSchedule().asString());
+    void receive(Transporter transporter) {
+        while (!transporter.isEmpty()) {
+
+            Shippable item = transporter.unload();
+            String medName = item.getMedicineName();
+            MedicineSchedule schedule = item.getSchedule();
+
+            System.out.println(String.format(
+                    "Checking whether Hospital can receive %s.",
+                    medName));
+
+            boolean isUncontrolled = (schedule == MedicineSchedule.Uncontrolled);
+
+            if (isUncontrolled) {
+                System.out.println(String.format(
+                        "Accepted a shipment of %s.",
+                        medName));
             } else {
-                System.out.printf("Accepted a shipment of %s.\n", unloaded.getMedicineName());
+                System.out.println(String.format(
+                        "Hospital cannot receive controlled substances and %s is a %s.",
+                        medName,
+                        schedule.asString()));
             }
         }
     }
